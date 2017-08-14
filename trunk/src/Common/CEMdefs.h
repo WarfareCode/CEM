6492350f89a7c04d90f6436759cc77f5afb125fa
frazibw1 <frazibw1@frazibw1-ml2.jhuapl.edu>
2017-08-14 08:24:00 -0400

    Adding Common Definitions file to hold standard electromagnetic constants like speed of light

---
+++ trunk/src/Common/CEMdefs.h
@@ -0,0 +1,12 @@
+/**
+* @file CEMdefs.h
+* @brief Header File with standard definitions *
+* @author Ben Frazier*
+* @date 08/14/2017 */
+
+namespace CEM
+{
+  const double speedOfLight = 299792458; /*!< speed of light in \f[\frac{m}{s}\f] */
+  const double epsilon0 = 8.85418782E-12;  /*!< permittivity of free space in \f[\frac{s^4A^2}{m^3kg}\f] */
+  const double mu0 = 1.25663706E-6; /*!< permeability of free space, in \f[\frac{m kg}{s^2A^2} \f] */
+}
