430df67563ed8510418cecca7a8b12b325e74f47
frazibw1 <frazibw1@frazibw1-ml2.jhuapl.edu>
2017-08-18 13:04:28 -0400

    Adding unit test case to change the vector size for writing

--- trunk/src/DataLogger/tests/DataLogger_test.cpp
+++ trunk/src/DataLogger/tests/DataLogger_test.cpp
@@ -7,6 +7,7 @@
   using ::testing::Eq;
 #include <gtest/gtest.h>
   using ::testing::Test;
+using ::testing::TestWithParam;
 
 #include "MockInputDataInterface.h"
 #include "CEMdefs.h"
@@ -18,7 +19,7 @@ namespace DataLogger_Test
 {
 namespace testing
 {
-    class DataLogger_Test : public Test
+  class DataLogger_Test : public TestWithParam<int>
     {
     protected:
        DataLogger_Test(){}
@@ -26,25 +27,25 @@ namespace testing
 
       virtual void SetUp()
       {
-       
 	input = new MockInputData;
-        EXPECT_CALL(*input, getVectorLength()).WillRepeatedly(::testing::Return(15));
 	EXPECT_CALL(*input, getOutputFileName()).WillRepeatedly(::testing::Return("CEMTest.h5"));
 	
-	dl = new DataLoggerHDF5(input);
-
       }
       virtual void TearDown(){ delete dl; delete input;}
 
-        DataLoggerHDF5* dl;
-        MockInputData* input;
-        std::string outputFileName;
+      DataLoggerHDF5* dl;
+      MockInputData* input;
+      std::string outputFileName;
+
+      int size;
 
     };
 
 
-    TEST_F(DataLogger_Test, write_read_vector)
+    TEST_F(DataLogger_Test, write_read_vector_fixed_size)
     {
+      EXPECT_CALL(*input, getVectorLength()).WillRepeatedly(::testing::Return(15));
+      dl = new DataLoggerHDF5(input);
       std::vector<double> in = {1,5.87,7.993,8,10.2,2.0190,30,50,100,150,-100,-20,-30.5, 3.4, 5.776};
       dl->WriteDataArray(in);
       std::vector<double> out = dl->ReadDataArray(input->getOutputFileName(),"/Ex", 0);
@@ -57,6 +58,35 @@ namespace testing
 	}
     
     }
+
+  TEST_P(DataLogger_Test, write_read_vector_variable_sizes)
+  {
+     size = GetParam();
+     EXPECT_CALL(*input, getVectorLength()).WillRepeatedly(::testing::Return(size));
+      dl = new DataLoggerHDF5(input);
+
+     std::vector<double>(in);
+     in.resize(size);
+     for (int i = 0; i < size; i++)
+       {
+	 in[i] = rand();
+       }
+
+      dl->WriteDataArray(in);
+
+     std::vector<double> out = dl->ReadDataArray(input->getOutputFileName(),"/Ex", 0);
+
+      EXPECT_THAT(in.size(),Eq(out.size()));
+      
+      for (int counter = 0; counter < in.size();counter++)
+	{
+	  EXPECT_THAT(in[counter], Eq(out[counter]));
+	}
+      
+    std::cout<<size <<std::endl;
+  }
+
+  INSTANTIATE_TEST_CASE_P(NewVectorSizes, DataLogger_Test,::testing::Values(5,7));//,10,30,50));
 
 } // namespace testing
 } // namespace DataLogger_Test
